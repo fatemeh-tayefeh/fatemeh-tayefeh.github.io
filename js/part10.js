317c93438552f0f94ededfce635f19051cc21982
fatemeh-tayefeh <fatemehtayefeh1378@gmail.com>
2024-12-17 16:57:03 +0330

    end321

---
+++ js/part10.js
@@ -0,0 +1,7 @@
+const ICON_10_1 = document.querySelector(".div-part10-1-icon");
+const NAV_PART10_1_JAY = document.querySelector(".div-part10-1-1");
+
+
+ICON_10_1.addEventListener("click", function () {
+    NAV_PART10_1_JAY.classList.toggle("div-part10-1_dakheli");
+});
